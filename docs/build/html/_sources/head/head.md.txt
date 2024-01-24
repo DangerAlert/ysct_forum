8f7b0a73d8390a166c1a3fdb43447723c633ef16
afan <bili_afan@163.com>
2024-01-24 14:45:06 +0800

    first commit

---
+++ docs/build/html/_sources/head/head.md.txt
@@ -0,0 +1,13 @@
+# 公司介绍
+
+<div align=center><img src="../_images/logo.png" width=40% height=40%></div>
+
+逸思长天（南京）数字智能科技有限公司旗下长天ML持续学习平台，更高程度的自动化机器学习，用户仅需提供训练数据，无需具备任何机器学习知识即可构建机器学习模型，并且随数据变化自动更新。让普通人的AI建模能力达到专家水平。加速各行各业智能化场景探索与落地。
+
+关注微信公众号【逸思长天】及时接收最新技术文章！
+
+<div align=center><img src="../_images/wechat.png" width=30% height=30%></div>
+
+关注B站账号【长天机器学习工具】及时接收最新技术文章！
+
+<div align=center><img src="../_images/bilibili.png" width=30% height=30%></div>
---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '长天机器学习平台介绍文档'
+copyright = '2023, liyifan'
+author = 'liyifan'
+release = '1.0.0'
+language = 'zh_CN'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# 导入readthedoc主题
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+
+
+# 增加扩展文档格式
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
